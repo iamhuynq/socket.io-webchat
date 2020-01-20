040b0e9008271b1d577ced0761f3b87dad39ced1
huycan511 <nguyenquanghuy0511@gmail.com>
2020-01-20 13:14:08 +0700

    add socket connection

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-var express = require('express');
+const express = require('express');
+const socket = require('socket.io');
+//App setup
+const app = express();
+const server = app.listen(4000, function(){
+    console.log('Listening to requests on port 4000...');
+});
+
+//Static files
+app.use(express.static('public'));
+
+//Socket setup
+const io = socket(server);
+
+io.on('connection', function(socket){
+    console.log(socket.id);
+})
